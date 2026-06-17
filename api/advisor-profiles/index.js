32eb15fdb5621670b2d789bb73588bf786613c2f
ThaoPT99 <phantruongthao199@gmail.com>
2026-06-17 10:44:21 +0700

    load data

--- api/advisor-profiles/index.js
+++ api/advisor-profiles/index.js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=86400');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'GET') {
